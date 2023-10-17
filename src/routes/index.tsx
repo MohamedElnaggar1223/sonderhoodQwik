9cfcf6f8585bb5e019e1c7a96d202e667c906452
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-10-17 22:19:01 +0300

    deploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,53 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import ClientsSay from "~/components/HomePage/ClientsSay";
+import FAQ from "~/components/HomePage/FAQ";
+import Home from "~/components/HomePage/Home";
+import LookingForHelp from "~/components/HomePage/LookingForHelp";
+import MeetTheTeam from "~/components/HomePage/MeetTheTeam";
+import OurBlog from "~/components/HomePage/OurBlog";
+import OurServices from "~/components/HomePage/OurServices";
+import TherapyImportance from "~/components/HomePage/TherapyImportance";
+import WhySonderHood from "~/components/HomePage/WhySonderHood";
 
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
-
-export default component$(() => {
-  return (
-    <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
-    </>
-  );
+export default component$(() => 
+{
+	return (
+		<>
+            <Home />
+            <TherapyImportance />
+            <OurServices />
+            <LookingForHelp />
+            <WhySonderHood />
+            <MeetTheTeam />
+            <OurBlog />
+            <ClientsSay />
+            <FAQ faqs={[]} />
+        </>
+	);
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Home",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "The Sonderhood Website",
     },
   ],
+  links: [
+	{
+		rel: 'preconnect',
+		href: "https://fonts.googleapis.com"
+	},
+	{
+		rel: 'preconnect',
+		href: "https://fonts.gstatic.com",
+		crossorigin: 'true'
+	},
+	{
+		rel: 'stylesheet',
+		href: "https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,100;9..40,200;9..40,300;9..40,400;9..40,500;9..40,600;9..40,700&display=swap"
+	}
+  ]
 };
